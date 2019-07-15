eecfdac0099a2f8b21d4d6264ed680efe65ed347
Haron <haronshaker@outlook.com>
2019-07-15 16:55:11 +0200

    second inital commit

---
+++ include/DijetAngularAnalysisSelections.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "UHH2/core/include/fwd.h"
+#include "UHH2/core/include/Selection.h"
+
+namespace uhh2examples {
+    
+/* Select events with at least two jets in which the leading two jets have deltaphi > 2.7 and the third jet pt is
+ * below 20% of the average of the leading two jets, where the minimum deltaphi and
+ * maximum third jet pt fraction can be changed in the constructor.
+ * The jets are assumed to be sorted in pt.
+ */
+class DijetSelection: public uhh2::Selection {
+public:
+    DijetSelection(float dphi_min = 2.7f, float third_frac_max = 0.2f);
+    virtual bool passes(const uhh2::Event & event) override;
+private:
+    float dphi_min, third_frac_max;
+};
+
+
+}
+
+
+//class DijetSelection2: public uhh2::Selection {
+//public:
+//    DijetSelection2(float rapi0_max = 2.5, float rapi1_max = 2.5, float pt_min=200, float yboost_min = 1.1, float chi_min = 1, float chi_max = 16);
+//    virtual bool passes(const uhh2::Event & event) override;
+//private:
+//    float rapi0_max, rapi1_max, pt_min, yboost_min, chi_min, chi_max;
+//};
+
+class Rapidity_sel: public uhh2::Selection {
+public:
+    Rapidity_sel(float rapi0_max = 2.5, float rapi1_max = 2.5);
+    virtual bool passes(const uhh2::Event & event) override;
+private:
+    float rapi0_max, rapi1_max;
+};
+
+
+class pT_sel: public uhh2::Selection {
+public:
+    pT_sel(float pt_min=200);
+    virtual bool passes(const uhh2::Event & event) override;
+private:
+    float  pt_min;
+};
+
+class chi_sel: public uhh2::Selection {
+public:
+    chi_sel(float chi_min = 1, float chi_max = 16);
+    virtual bool passes(const uhh2::Event & event) override;
+private:
+    float chi_min, chi_max;
+};
+
+class yboost_sel: public uhh2::Selection {
+public:
+    yboost_sel(float yboost_min = 1.1);
+    virtual bool passes(const uhh2::Event & event) override;
+private:
+    float yboost_min;
+};
+
+
+
+//class MjjSelection1: public uhh2::Selection {
+//public:
+//    MjjSelection1(float energy_min = 1.9, energy_max = 2.4);
+//    virtual bool passes(const uhh2::Event & event) override;
+//private:
+//    float energy_min, energy_max;
+//};
---
+++ src/DijetAngularAnalysisSelections.cxx
@@ -0,0 +1,113 @@
+#include "UHH2/DijetAngularAnalysis/include/DijetAngularAnalysisSelections.h"
+#include "UHH2/core/include/Event.h"
+
+#include <stdexcept>
+
+using namespace uhh2examples;
+using namespace uhh2;
+
+
+DijetSelection::DijetSelection(float dphi_min_, float third_frac_max_): dphi_min(dphi_min_), third_frac_max(third_frac_max_){}
+    
+bool DijetSelection::passes(const Event & event){
+    assert(event.jets); // if this fails, it probably means jets are not read in
+    if(event.jets->size() < 2) return false;
+    const auto & jet0 = event.jets->at(0);
+    const auto & jet1 = event.jets->at(1); 
+    auto dphi = deltaPhi(jet0, jet1);
+    if(dphi < dphi_min) return false;
+    if(event.jets->size() == 2) return true;
+    const auto & jet2 = event.jets->at(2);
+    auto third_jet_frac = jet2.pt() / (0.5 * (jet0.pt() + jet1.pt()));
+    return third_jet_frac < third_frac_max;
+}
+
+//DijetSelection2::DijetSelection2(float rapi0_, float rapi1_, float pt, float chi_mini, float chi_maxi, float yboost_): rapi0_max(rapi0_), rapi1_max(rapi1_), pt_min(pt), chi_min(chi_mini), chi_max(chi_maxi), yboost_min(yboost_){}
+
+//bool DijetSelection2::passes(const Event & event){
+	//assert(event.jets);
+	//if(event.jets->size() < 2) return false;
+	//const auto & rapi0 = 0.5*TMath::Log((event.jets->at(0).energy()+event.jets->at(0).v4().Pz())/(event.jets->at(0).energy()-event.jets->at(0).v4().Pz()));
+	//const auto & rapi1 = 0.5*TMath::Log((event.jets->at(1).energy()+event.jets->at(1).v4().Pz())/(event.jets->at(1).energy()-event.jets->at(1).v4().Pz()));
+	//if(abs(rapi0) > rapi0_max) return false;
+	//if(abs(rapi1) > rapi1_max) return false;
+	//const auto & jet0 = event.jets->at(0);
+	//const auto & jet1 = event.jets->at(1); 
+	//auto pt1 = jet0.pt();
+	//auto pt2 = jet1.pt();
+	//if(pt1 < pt_min) return false;
+	//if(pt2 < pt_min) return false;
+	//auto chi_ = TMath::Exp(abs((rapi0)-(rapi1)));
+	//if((chi_ < chi_min) && (chi_ > chi_max)) return false;
+	//auto yboost_ = 0.5*((rapi0)+(rapi1)); 
+	//if(yboost_ > yboost_min) return false;
+	//return true;
+//}
+
+
+
+
+Rapidity_sel::Rapidity_sel(float rapi0_, float rapi1_): rapi0_max(rapi0_), rapi1_max(rapi1_){}
+
+bool Rapidity_sel::passes(const Event & event){
+	assert(event.jets);
+	if(event.jets->size() < 2) return false;
+	const auto & rapi0 = 0.5*TMath::Log((event.jets->at(0).energy()+event.jets->at(0).v4().Pz())/(event.jets->at(0).energy()-event.jets->at(0).v4().Pz()));
+	const auto & rapi1 = 0.5*TMath::Log((event.jets->at(1).energy()+event.jets->at(1).v4().Pz())/(event.jets->at(1).energy()-event.jets->at(1).v4().Pz()));
+	if(abs(rapi0) > rapi0_max) return false;
+	if(abs(rapi1) > rapi1_max) return false;
+
+	return true;
+}
+
+pT_sel::pT_sel(float pt):  pt_min(pt){}
+
+bool pT_sel::passes(const Event & event){
+	assert(event.jets);
+	if(event.jets->size() < 2) return false;
+	const auto & jet0 = event.jets->at(0);
+	const auto & jet1 = event.jets->at(1); 
+	auto pt1 = jet0.pt();
+	auto pt2 = jet1.pt();
+	if(pt1 < pt_min) return false;
+	if(pt2 < pt_min) return false;
+	return true;
+}
+
+chi_sel::chi_sel(float chi_mini, float chi_maxi): chi_min(chi_mini), chi_max(chi_maxi){}
+
+bool chi_sel::passes(const Event & event){
+	assert(event.jets);
+	if(event.jets->size() < 2) return false;
+	const auto & rapi0 = 0.5*TMath::Log((event.jets->at(0).energy()+event.jets->at(0).v4().Pz())/(event.jets->at(0).energy()-event.jets->at(0).v4().Pz()));
+	const auto & rapi1 = 0.5*TMath::Log((event.jets->at(1).energy()+event.jets->at(1).v4().Pz())/(event.jets->at(1).energy()-event.jets->at(1).v4().Pz()));
+	auto chi_ = TMath::Exp(abs((rapi0)-(rapi1)));
+	if((chi_ < chi_min) || (chi_ > chi_max)) return false;
+	return true;
+}
+
+
+yboost_sel::yboost_sel(float yboost): yboost_min(yboost){}
+
+bool yboost_sel::passes(const Event & event){
+	assert(event.jets);
+	if(event.jets->size() < 2) return false;
+	const auto & rapi0 = 0.5*TMath::Log((event.jets->at(0).energy()+event.jets->at(0).v4().Pz())/(event.jets->at(0).energy()-event.jets->at(0).v4().Pz()));
+	const auto & rapi1 = 0.5*TMath::Log((event.jets->at(1).energy()+event.jets->at(1).v4().Pz())/(event.jets->at(1).energy()-event.jets->at(1).v4().Pz()));
+	auto yboost_ = 0.5*((rapi0)+(rapi1)); 
+	if(yboost_ > yboost_min) return false;
+	return true;
+}
+
+
+
+//MjjSelection1::MjjSelection1(float energy_j1, float energy_j2): energy1(energy_j1), energy2(energy_j2)({}
+//
+//bool MjjSelection1::passes(const Event & event){
+//	assert(event.jets)
+//	if(event.jets->size() < 2) return false;
+//	auto energy1 = event.jets->at(0).energy()
+//	auto energy2 = event.jets->at(1).energy()
+//	if((energy_j1 < energy_min) && (energy_j2 > energy_max)) return false
+	
+	
